0b8c85c359b893dd9033de13ad353056293dfb72
hunjin <hunivers0523@naver.com>
2025-01-23 23:32:10 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    
+    let size = numbers.length;
+    
+    numbers.sort((a,b) => a -b);
+    
+    return Math.max(numbers[0] * numbers[1],
+                   numbers[size - 1] * numbers[size- 2]);
+}
