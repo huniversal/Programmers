53ebdf28c88e41e46f71a4adeaf6ec6274053fce
hunjin lee <hunivers0523@naver.com>
2025-03-31 01:07:41 +0900

    [Bronze V] Title: 사칙연산, Time: 96 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,10 @@
+var fs = require("fs");
+var filedata = fs.readFileSync(0).toString().split(" ");
+const A = parseInt(filedata[0]);
+const B = parseInt(filedata[1]);
+
+console.log(A + B);
+console.log(A - B);
+console.log(A * B);
+console.log(parseInt(A / B));
+console.log(A % B);
