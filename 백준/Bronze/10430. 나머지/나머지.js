43a9182877f6152c4c64ae01f62c39f70796f9f1
hunjin lee <hunivers0523@naver.com>
2025-03-31 01:19:47 +0900

    [Bronze V] Title: 나머지, Time: 92 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,10 @@
+var fs = require("fs");
+var filedata = fs.readFileSync(0).toString().split(" ");
+const A = parseInt(filedata[0]);
+const B = parseInt(filedata[1]);
+const C = parseInt(filedata[2]);
+
+console.log((A + B) % C);
+console.log(((A % C) + (B % C)) % C);
+console.log((A * B) % C);
+console.log(((A % C) * (B % C)) % C);
