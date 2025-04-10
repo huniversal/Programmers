a7c4b347842b13e6a571f4a09da070cdb7eb1481
hunjin lee <hunivers0523@naver.com>
2025-04-10 11:12:38 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 88 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.js
@@ -0,0 +1,28 @@
+
+function main() {
+  const data = getData();
+  const bytes = data[0][0];
+  const longTimes = bytes / 4;
+  let result = "long ".repeat(longTimes);
+
+  console.log(result + "int");
+}
+main();
+
+function getData() {
+  const fs = require("fs");
+  const fileData = fs.readFileSync(0).toString();
+  const arr = fileData.trim().split("\n");
+
+  const result = []; // 리턴할 2차원 배열
+
+  for (let row of arr) {
+    // '23 48', '25'
+    const rowArr = row.split(" "); // ['23', '48'], ['25']
+    for (let k = 0; k < rowArr.length; k++) {
+      rowArr[k] = isNaN(rowArr[k]) ? rowArr[k] : parseInt(rowArr[k]);
+    }
+    result.push(rowArr);
+  }
+  return result;
+}
